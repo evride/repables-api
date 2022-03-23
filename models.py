5ceb6caa0ddef2263541a3396275f593be694a1a
Zeon Stewart <zeonstewart23@gmail.com>
2022-03-23 23:14:00 +0000

    Add item create and upload endpoints, user response models to remove private fields, image resizing

--- models.py
+++ models.py
@@ -20,7 +20,6 @@ class User(Model):
         company = CharField(max_length = 128, null=True)
         biography = TextField(null=True)
         website = TextField(null=True)
-        tags = TextField(null=True)
         birthdate = DateTimeField(null=True)
         email_public = BooleanField(default=False)
         display_birthday = BooleanField(default=False)
@@ -59,12 +58,12 @@ class Item(Model):
 class ItemRevision(Model):
         item = ForeignKeyField(Item, null=True)
         user = ForeignKeyField(User)
-        name = CharField(max_length=255)
-        description = TextField(null=True)
-        instructions = TextField(null=True)
-        tags = TextField(null=True)
-        license = CharField(max_length=10) #Enum
-        version = CharField(max_length=50)
+        name = CharField(max_length=255, default="")
+        description = TextField(null=True, default="")
+        instructions = TextField(null=True, default="")
+        tags = TextField(null=True, default="")
+        license = CharField(max_length=10, default="") #Enum
+        version = CharField(max_length=50, default="")
         deleted_at = DateTimeField(null=True)
         created_at = DateTimeField(default=datetime.now)
         updated_at = DateTimeField(default=datetime.now)
@@ -81,6 +80,7 @@ class ItemUpload(Model):
     file_extension = CharField(max_length=16, null=True) #adding the nullable for testing
     size = IntegerField()#filesize Number (file size in number of bytes)
     mimetype = CharField(max_length=64)#mimetype  varchar(64) NULLABLE (ex: image/png)
+    revision_id = ForeignKeyField(ItemRevision)
 	#type varchar(1) NULLABLE (either "primary", "secondary") //define it but mostly ignore for now
 	#hash String(40) NULLABLE (file hash, md5 ) //define but can ignore for now
 	#enabled Boolean
@@ -95,3 +95,8 @@ class ItemUpload(Model):
 
     def __str__(self):
         return self.filename
+
+# class PasswordReset(Model):
+#     user = ForeignKeyField(User)
+#     reset_key = CharField(max_length=256)
+#     created_at = DateTimeField(default=datetime.now)
